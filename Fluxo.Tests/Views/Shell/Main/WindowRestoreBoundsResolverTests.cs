edc90cb66c198085bf30135d13e75f609224433b
Hokichi <hokichi141@gmail.com>
2026-04-18 15:16:25 +0700

    Fixed window bouncing back to previous monitor

---
+++ Fluxo.Tests/Views/Shell/Main/WindowRestoreBoundsResolverTests.cs
@@ -0,0 +1,34 @@
+using System.Windows;
+using Fluxo.Views.Shell.Main;
+using Xunit;
+
+namespace Fluxo.Tests.Views.Shell.Main;
+
+public class WindowRestoreBoundsResolverTests
+{
+    [Fact]
+    public void ResolveCenteredRestoreBounds_CentersOnSecondaryMonitorWorkArea()
+    {
+        var workArea = new Rect(1920, 0, 1920, 1040);
+
+        var bounds = WindowRestoreBoundsResolver.ResolveCenteredRestoreBounds(workArea);
+
+        Assert.Equal(2080, bounds.Left);
+        Assert.Equal(70, bounds.Top);
+        Assert.Equal(1600, bounds.Width);
+        Assert.Equal(900, bounds.Height);
+    }
+
+    [Fact]
+    public void ResolveCenteredRestoreBounds_UsesWorkAreaOriginForExactFit()
+    {
+        var workArea = new Rect(2560, 1440, 1600, 900);
+
+        var bounds = WindowRestoreBoundsResolver.ResolveCenteredRestoreBounds(workArea);
+
+        Assert.Equal(2560, bounds.Left);
+        Assert.Equal(1440, bounds.Top);
+        Assert.Equal(1600, bounds.Width);
+        Assert.Equal(900, bounds.Height);
+    }
+}
