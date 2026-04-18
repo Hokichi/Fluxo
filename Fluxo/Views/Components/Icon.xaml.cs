060ffd94ecf461e5401dc1aa70782a5c28eb5be0
Hokichi <hokichi141@gmail.com>
2026-04-18 12:33:17 +0700

    Changed ViewBoxes to Icon

---
+++ Fluxo/Views/Components/Icon.xaml.cs
@@ -0,0 +1,37 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Fluxo.Views.Components;
+
+public partial class Icon : UserControl
+{
+    public static readonly DependencyProperty PathProperty = DependencyProperty.Register(
+        nameof(Path),
+        typeof(Geometry),
+        typeof(Icon),
+        new PropertyMetadata(null));
+
+    public static readonly DependencyProperty ColorProperty = DependencyProperty.Register(
+        nameof(Color),
+        typeof(Brush),
+        typeof(Icon),
+        new PropertyMetadata(Brushes.Black));
+
+    public Icon()
+    {
+        InitializeComponent();
+    }
+
+    public Geometry? Path
+    {
+        get => (Geometry?)GetValue(PathProperty);
+        set => SetValue(PathProperty, value);
+    }
+
+    public Brush? Color
+    {
+        get => (Brush?)GetValue(ColorProperty);
+        set => SetValue(ColorProperty, value);
+    }
+}
