d9170be45c8a1171ae51d5f350062b30f72a8452
Hokichi <hokichi141@gmail.com>
2026-04-19 14:22:42 +0700

    Fixed inconsistent icons usage

--- Fluxo/Views/Components/Icon.xaml.cs
+++ Fluxo/Views/Components/Icon.xaml.cs
@@ -1,6 +1,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace Fluxo.Views.Components;
 
@@ -12,9 +13,9 @@ public partial class Icon : UserControl
         typeof(Icon),
         new PropertyMetadata(null, OnPathChanged));
 
-    public static readonly DependencyProperty MaskDrawingProperty = DependencyProperty.Register(
-        nameof(MaskDrawing),
-        typeof(Drawing),
+    public static readonly DependencyProperty MaskBrushProperty = DependencyProperty.Register(
+        nameof(MaskBrush),
+        typeof(Brush),
         typeof(Icon),
         new PropertyMetadata(null));
 
@@ -35,10 +36,10 @@ public partial class Icon : UserControl
         set => SetValue(PathProperty, value);
     }
 
-    public Drawing? MaskDrawing
+    public Brush? MaskBrush
     {
-        get => (Drawing?)GetValue(MaskDrawingProperty);
-        private set => SetValue(MaskDrawingProperty, value);
+        get => (Brush?)GetValue(MaskBrushProperty);
+        private set => SetValue(MaskBrushProperty, value);
     }
 
     public Brush? Color
@@ -49,13 +50,32 @@ public partial class Icon : UserControl
 
     private static void OnPathChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
-        ((Icon)d).UpdateMaskDrawing();
+        ((Icon)d).UpdateMaskBrush();
     }
 
-    private void UpdateMaskDrawing()
+    private void UpdateMaskBrush()
     {
         var geometry = ResolveGeometry(Path);
-        MaskDrawing = geometry is null ? null : new GeometryDrawing(Brushes.Black, null, geometry);
+        if (geometry is not null)
+        {
+            MaskBrush = new DrawingBrush(new GeometryDrawing(Brushes.Black, null, geometry))
+            {
+                Stretch = Stretch.Uniform
+            };
+            return;
+        }
+
+        var imageSource = ResolveImageSource(Path);
+        if (imageSource is not null)
+        {
+            MaskBrush = new ImageBrush(imageSource)
+            {
+                Stretch = Stretch.Uniform
+            };
+            return;
+        }
+
+        MaskBrush = null;
     }
 
     private static Geometry? ResolveGeometry(object? source)
@@ -75,6 +95,29 @@ public partial class Icon : UserControl
                 }
 
                 break;
+        }
+
+        return null;
+    }
+
+    private static ImageSource? ResolveImageSource(object? source)
+    {
+        switch (source)
+        {
+            case ImageSource imageSource:
+                return imageSource;
+            case Uri uri:
+                return new BitmapImage(uri);
+            case string path when !string.IsNullOrWhiteSpace(path):
+                try
+                {
+                    var converter = new ImageSourceConverter();
+                    return converter.ConvertFromInvariantString(path) as ImageSource;
+                }
+                catch
+                {
+                    return null;
+                }
         }
 
         return null;
