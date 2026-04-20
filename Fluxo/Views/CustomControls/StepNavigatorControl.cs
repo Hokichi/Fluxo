1b0f9d7c41007ed7820c248207b3d89d86fb50f6
Hokichi <hokichi141@gmail.com>
2026-04-20 14:03:17 +0700

    feat: implement StepNavigatorControl with UpdateDotStates

---
+++ Fluxo/Views/CustomControls/StepNavigatorControl.cs
@@ -0,0 +1,87 @@
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using Fluxo.ViewModels.CustomControls;
+
+namespace Fluxo.Views.CustomControls;
+
+public sealed class StepNavigatorControl : Control
+{
+    static StepNavigatorControl()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(
+            typeof(StepNavigatorControl),
+            new FrameworkPropertyMetadata(typeof(StepNavigatorControl)));
+    }
+
+    public static readonly DependencyProperty StepCountProperty =
+        DependencyProperty.Register(
+            nameof(StepCount),
+            typeof(int),
+            typeof(StepNavigatorControl),
+            new PropertyMetadata(0, OnStepCountChanged));
+
+    public static readonly DependencyProperty CurrentStepProperty =
+        DependencyProperty.Register(
+            nameof(CurrentStep),
+            typeof(int),
+            typeof(StepNavigatorControl),
+            new PropertyMetadata(0, OnCurrentStepChanged));
+
+    private static readonly DependencyPropertyKey DotsPropertyKey =
+        DependencyProperty.RegisterReadOnly(
+            nameof(Dots),
+            typeof(ObservableCollection<StepNavigatorDotVM>),
+            typeof(StepNavigatorControl),
+            new PropertyMetadata(null));
+
+    public static readonly DependencyProperty DotsProperty = DotsPropertyKey.DependencyProperty;
+
+    public StepNavigatorControl()
+    {
+        SetValue(DotsPropertyKey, new ObservableCollection<StepNavigatorDotVM>());
+    }
+
+    public int StepCount
+    {
+        get => (int)GetValue(StepCountProperty);
+        set => SetValue(StepCountProperty, value);
+    }
+
+    public int CurrentStep
+    {
+        get => (int)GetValue(CurrentStepProperty);
+        set => SetValue(CurrentStepProperty, value);
+    }
+
+    public ObservableCollection<StepNavigatorDotVM> Dots =>
+        (ObservableCollection<StepNavigatorDotVM>)GetValue(DotsProperty);
+
+    private static void OnStepCountChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var control = (StepNavigatorControl)d;
+        var dots = control.Dots;
+        dots.Clear();
+
+        var count = (int)e.NewValue;
+        for (var i = 0; i < count; i++)
+            dots.Add(new StepNavigatorDotVM { IsFirst = i == 0 });
+
+        UpdateDotStates(dots, control.CurrentStep);
+    }
+
+    private static void OnCurrentStepChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var control = (StepNavigatorControl)d;
+        UpdateDotStates(control.Dots, (int)e.NewValue);
+    }
+
+    internal static void UpdateDotStates(ObservableCollection<StepNavigatorDotVM> dots, int currentStep)
+    {
+        for (var i = 0; i < dots.Count; i++)
+        {
+            dots[i].IsCompleted = i < currentStep - 1;
+            dots[i].IsActive = i == currentStep - 1;
+        }
+    }
+}
