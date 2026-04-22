040b1b90842e77c78b53e9328616de930068ac73
Hokichi <hokichi141@gmail.com>
2026-04-22 11:48:07 +0700

    Added NotificationItemVM

---
+++ Fluxo/ViewModels/Shell/Main/NotificationItemVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Fluxo.ViewModels.Entities;
+
+namespace Fluxo.ViewModels.Shell.Main
+{
+    public partial class NotificationItemVM : ObservableObject
+    {
+        [ObservableProperty] private ObservableCollection<NotificationVM> _notifications;
+        [ObservableProperty] private string _message;
+        [ObservableProperty] private int _count;
+    }
+}
