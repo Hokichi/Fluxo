32e7c87741f3d102b7bb017dcaf7054d7575fa8e
Hokichi <hokichi141@gmail.com>
2026-04-25 21:19:18 +0700

    test: harden mainwindow layout analytics guards

--- Fluxo.Tests/Views/Shell/Main/MainWindowLayoutTests.cs
+++ Fluxo.Tests/Views/Shell/Main/MainWindowLayoutTests.cs
@@ -1,32 +1,89 @@
 using System.IO;
+using System.Linq;
+using System.Xml.Linq;
 using Xunit;
 
 namespace Fluxo.Tests.Views.Shell.Main;
 
 public sealed class MainWindowLayoutTests
 {
-    private static readonly string MainWindowXamlPath = Path.GetFullPath(Path.Combine(
-        AppContext.BaseDirectory,
-        "..", "..", "..", "..", "..",
-        "Fluxo",
-        "Views",
-        "Shell",
-        "Main",
-        "MainWindow.xaml"));
+    private static readonly XNamespace PresentationNamespace = "http://schemas.microsoft.com/winfx/2006/xaml/presentation";
+    private static readonly XNamespace XamlNamespace = "http://schemas.microsoft.com/winfx/2006/xaml";
+    private static readonly Lazy<string> MainWindowXaml = new(LoadMainWindowXaml);
+    private static readonly Lazy<XDocument> MainWindowXamlDocument = new(() => XDocument.Parse(MainWindowXaml.Value));
 
     [Fact]
     public void HeaderMenu_DoesNotExposeAnalyticsActionButton()
     {
-        var xaml = File.ReadAllText(MainWindowXamlPath);
+        var xaml = MainWindowXaml.Value;
+        var xamlDocument = MainWindowXamlDocument.Value;
 
         Assert.DoesNotContain("Click=\"OnAnalyticsButtonClick\"", xaml);
+
+        var headerMenuPopup = xamlDocument
+            .Descendants(PresentationNamespace + "Popup")
+            .SingleOrDefault(popup => (string?)popup.Attribute(XamlNamespace + "Name") == "HeaderMenuPopup");
+
+        Assert.NotNull(headerMenuPopup);
+
+        var analyticsActionEntry = headerMenuPopup!
+            .Descendants(PresentationNamespace + "Button")
+            .SingleOrDefault(button => (string?)button.Attribute("Click") == "OnAnalyticsButtonClick");
+
+        Assert.Null(analyticsActionEntry);
     }
 
     [Fact]
     public void AnalyticsDrawerTabTrigger_RemainsAvailable()
     {
-        var xaml = File.ReadAllText(MainWindowXamlPath);
+        var xaml = MainWindowXaml.Value;
+        var xamlDocument = MainWindowXamlDocument.Value;
 
         Assert.Contains("Click=\"OnAnalyticsDrawerTabClick\"", xaml);
+
+        var analyticsDrawerTabButton = xamlDocument
+            .Descendants(PresentationNamespace + "Button")
+            .SingleOrDefault(button => (string?)button.Attribute(XamlNamespace + "Name") == "AnalyticsDrawerTabButton");
+
+        Assert.NotNull(analyticsDrawerTabButton);
+        Assert.Equal("OnAnalyticsDrawerTabClick", (string?)analyticsDrawerTabButton!.Attribute("Click"));
+    }
+
+    private static string LoadMainWindowXaml()
+    {
+        var mainWindowXamlPath = ResolveMainWindowXamlPath();
+        return File.ReadAllText(mainWindowXamlPath);
+    }
+
+    private static string ResolveMainWindowXamlPath()
+    {
+        var currentDirectory = new DirectoryInfo(AppContext.BaseDirectory);
+
+        while (currentDirectory is not null)
+        {
+            var solutionPath = Path.Combine(currentDirectory.FullName, "Fluxo.sln");
+            if (File.Exists(solutionPath))
+            {
+                var mainWindowXamlPath = Path.Combine(
+                    currentDirectory.FullName,
+                    "Fluxo",
+                    "Views",
+                    "Shell",
+                    "Main",
+                    "MainWindow.xaml");
+
+                if (!File.Exists(mainWindowXamlPath))
+                {
+                    throw new FileNotFoundException($"MainWindow.xaml was not found at '{mainWindowXamlPath}'.", mainWindowXamlPath);
+                }
+
+                return mainWindowXamlPath;
+            }
+
+            currentDirectory = currentDirectory.Parent;
+        }
+
+        throw new DirectoryNotFoundException(
+            $"Could not locate repository root containing 'Fluxo.sln' from '{AppContext.BaseDirectory}'.");
     }
 }
